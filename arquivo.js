ae3386b26b6324d7c94988fd425cb56da398b682
fellipe30k <fellipejose2009@hotmail.com>
2022-01-20 18:44:59 -0300

    Revert "Merge pull request #2 from fellipe30k/feature-0001"
    This reverts commit 610bdb61d3c3086edcacb3079f9c66e31e448bd1, reversing
    changes made to 7ffceab3e5525f0fc3e4bbcaa104b490e4e9e052.

--- arquivo.js
+++ arquivo.js
@@ -2,9 +2,6 @@ import { Controller } from 'stimulus';
 export default class extends Controller {
     connect() {
         console.log('?');
-        var bla = 'blabla'
-        if (bla == 'blabla'){
-            return true
-        }
+        
     }
 }
